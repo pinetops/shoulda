46615dd8d55edc42eda398e07ba7496715a4ee80
Josh Nichols <josh@technicalpickles.com>
2008-09-12 13:47:16 -0400

    Moved all the rails initializiation type stuff to shoulda/rails. init.rb now includes that.

--- lib/shoulda/rails.rb
+++ lib/shoulda/rails.rb
@@ -1,7 +1,4 @@
-require 'rubygems'
-require 'active_support'
 require 'shoulda'
-
 require 'shoulda/active_record' if defined? ActiveRecord::Base
 require 'shoulda/controller'    if defined? ActionController::Base
 require 'shoulda/action_mailer' if defined? ActionMailer::Base
